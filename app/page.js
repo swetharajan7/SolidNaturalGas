45dca12deeec3a6ec39af521b0d8b6f05a9d108f
SR <90604384+swetharajan7@users.noreply.github.com>
2026-09-19 13:01:32 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -517,8 +517,7 @@ export default function Home() {
               <ul style={{ paddingLeft: "20px", margin: 0 }}>
                 {sources.map((source) => (
                   <li key={source.id} style={{ marginBottom: "6px" }}>
-                    
-                      href={source.url}
+                      <a href={source.url}
                       target="_blank"
                       rel="noopener noreferrer"
                       style={{ color: "#0B1F3B" }}
