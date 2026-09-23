82fe2ca6db845aad98b36fcd79b860b55b78bd16
SR <90604384+swetharajan7@users.noreply.github.com>
2026-09-23 20:04:22 +0530

    Update Sidebar.js

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -8,7 +8,8 @@ import { usePathname } from "next/navigation";
  * CHANGE THIS if your logo lives somewhere else.
  * Copy the src from the <img> in your old NavBar.js.
  */
-const LOGO_SRC = "/logo.png";
+const LOGO_SRC =           <Image src="/logo.png" alt="Solid Natural Gas" width={34} height={34} priority />
+;
 
 export const SIDEBAR_WIDTH = 260;
 
